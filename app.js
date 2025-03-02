c34076b442a9054eb36c6bfa650516cee989726e
Benzenism <68996721+Benzenism@users.noreply.github.com>
2025-03-02 21:07:53 +0900

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => console.log('Service Worker 등록 성공:', registration))
-        .catch(error => console.log('Service Worker 등록 실패:', error));
-}
