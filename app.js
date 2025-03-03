7189f67dc0b6ab1c21e7239c457ad691b4a9a20e
Benzenism <68996721+Benzenism@users.noreply.github.com>
2025-03-03 19:52:14 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => console.log('Service Worker 등록 성공:', registration))
-        .catch(error => console.log('Service Worker 등록 실패:', error));
+        .then(registration => console.log('Service Worker Regist Succeed', registration))
+        .catch(error => console.log('Service Worker Regist Failed', error));
 }
--- service_worker.js
+++ service_worker.js
@@ -10,11 +10,11 @@ const urlsToCache = [ // 캐시할 파일 선언
 
 // 서비스워커 설치 및 캐시 저장
 self.addEventListener('install', pEvent => {
-  console.log('서비스워커 설치함!');
+  console.log('Service Worker Installed');
   pEvent.waitUntil(
     caches.open(sCacheName)
     .then(pCache => {
-      console.log('파일을 캐시에 저장함!');
+      console.log('Files Saved into Cache');
       return pCache.addAll(urlsToCache);
     })
   );
@@ -22,7 +22,7 @@ self.addEventListener('install', pEvent => {
 
 // 고유번호 할당받은 서비스 워커 동작 시작
 self.addEventListener('activate', pEvent => {
-  console.log('서비스워커 동작 시작됨!');
+  console.log('Service Worker Activated');
 });
 
 // 데이터 요청시 네트워크 또는 캐시에서 찾아 반환 
@@ -31,10 +31,10 @@ self.addEventListener('fetch', pEvent => {
     caches.match(pEvent.request)
     .then(response => {
       if (!response) {
-        console.log("네트워크에서 데이터 요청!", pEvent.request)
+        console.log("Data Request from Network", pEvent.request)
         return fetch(pEvent.request);
       }
-      console.log("캐시에서 데이터 요청!", pEvent.request)
+      console.log("Data Request from Cache", pEvent.request)
       return response;
     }).catch(err => console.log(err))
   );
