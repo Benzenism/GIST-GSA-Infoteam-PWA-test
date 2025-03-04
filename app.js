2a55fe242720ce2decc345dc284f060d04fd5a91
Benzenism <68996721+Benzenism@users.noreply.github.com>
2025-03-04 20:13:25 +0900

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => console.log('Service Worker Regist Succeed', registration))
-        .catch(error => console.log('Service Worker Regist Failed', error));
-}
