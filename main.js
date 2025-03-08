063a382893ee6f1776298125b9d32439f6855a75
Benzenism <68996721+Benzenism@users.noreply.github.com>
2025-03-08 16:52:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -49,26 +49,27 @@ function calculateDistance(userLat, userLon) {
 
 // 식사할 장소 대리 선정
 function selectRestaurant(dist1, dist2){
-    if (distanceTo1 > 1000 || distanceTo2 > 1000) {
-        console.log('Too Far!');
-    } else {
-        const weight1 = 1 - (distanceTo1 * 0.001) + 0.3; // 락락
-        const weight2 = 1 - (distanceTo1 * 0.001) + 0.2; // 1학
-        const weight3 = 1 - (distanceTo2 * 0.001) + 0.1; // 2학
-        const weightSum = weight1 + weight2 + weight3
+    const textElement = document.getElementById('randomResult');
 
-        const textElement = document.getElementById('randomResult');
+    if (dist1 > 1000 || dist2 > 1000) {
+        console.log('Too Far!');
+        textElement.innerText = `GIST 밖이라면, 주변 식당에서 식사해보세요.`;
+    } else {
+        const weight1 = 1 - (dist1 * 0.001) + 0.3; // 락락
+        const weight2 = 1 - (dist1 * 0.001) + 0.2; // 1학
+        const weight3 = 1 - (dist2 * 0.001) + 0.1; // 2학
+        const weightSum = weight1 + weight2 + weight3
 
         const randomValue = Math.random(); // 소숫점 관련 오류는 무시
         if (randomValue <= weight1 / weightSum) {
-            console.log(`선택된 지점: 락락, 거리: ${distanceTo1} m`);
-            textElement.innerText = `선택된 지점: 락락(제 1학생회관 2층), 거리: ${distanceTo1.toFixed(1)} m`;
+            console.log(`선택된 지점: 락락, 거리: ${dist} m`);
+            textElement.innerText = `락락(제 1학생회관 2층), 거리: ${dist.toFixed(1)} m`;
         } else if (randomValue <= (weight1 + weight2) / weightSum) {
-            console.log(`선택된 지점: 1학, 거리: ${distanceTo1} m`);
-            textElement.innerText = `선택된 지점: 제 1학생회관, 거리: ${distanceTo1.toFixed(1)} m`;
+            console.log(`선택된 지점: 1학, 거리: ${dist1} m`);
+            textElement.innerText = `제 1학생회관, 거리: ${dist1.toFixed(1)} m`;
         } else {
-            console.log(`선택된 지점: 2학, 거리: ${distanceTo2} m`);
-            textElement.innerText = `선택된 지점: 제 2학생회관, 거리: ${distanceTo2.toFixed(1)} m`;
+            console.log(`선택된 지점: 2학, 거리: ${dist2} m`);
+            textElement.innerText = `제 2학생회관, 거리: ${dist2.toFixed(1)} m`;
         }
     }
 }
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const sCacheName = 'hello-pwa-v2'; // 캐시의 제목과 버전 선언
+const sCacheName = 'hello-pwa-v0'; // 캐시의 제목과 버전 선언
 const urlsToCache = [ // 캐시할 파일 선언
   './',
   './index.html',
